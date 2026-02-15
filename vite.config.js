beb48355f47efbdbf9eecdb0376dd669f5205e5e
rajputayus7 <ayush.rajput@applore.in>
2026-02-15 14:35:10 +0530

    lint added

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
+    exclude: [...configDefaults.exclude, 'src/tests/e2e/**']
   }
 })
