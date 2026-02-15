52259c39278a78d6774fb39e6b7ca580d6a1c24b
rajputayus7 <ayush.rajput@applore.in>
2026-02-15 14:16:17 +0530

    test file added , ci/cd also

---
+++ src/tests/app.test.jsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+
+
+import { render, screen } from '@testing-library/react'
+import App from '../App'
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Vite \+ React/i);
+  expect(linkElement).toBeInTheDocument();
+});
